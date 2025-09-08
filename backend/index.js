60ae2c82b77c0fbe7320e263cc3b03fdf6ce7e6c
DOC Analytica <docanalytica@gmail.com>
2025-09-08 13:14:37 +0530

    login issues fix 8/9 3

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,9 @@ if (process.env.NODE_ENV !== 'production') {
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://claims-site.vercel.app'
+}));
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
